18cf75761c3b884223114a47d900f48b48433e71
ajiagahari <pembikinjanda@gmail.com>
2018-07-22 02:34:36 +0700

    added assets and font

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import 'waypoints';
 import 'scrollTo';
-
+import 'bootstrap-daterangepicker';
 /**
  * EXAMPLE JS STARTS
  */
@@ -13,7 +13,8 @@ $(function() {
         var id = $(this).attr('id').slice(9);
         $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
     });
-
+    $('.date').daterangepicker({
+    singleDatePicker: true});
     $('#marketing').waypoint(function() {
         $('.img-circle').addClass('animated zoomIn');
     }, {
